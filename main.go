58399716cf5b75aca802c6e100e8bb5e08355a80
Toyin96 <onagoruwam@gmail.com>
2021-07-22 15:59:23 +0100

    added newsAPI client and its credentials

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
+	"fmt"
 	"github.com/joho/godotenv"
+	"go_news_app_git/news"
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
+	"time"
 )
 
 var tpl *template.Template
+var newsClient *news.Client
 
 func init(){
 	tpl = template.Must(template.ParseFiles("index.html"))
@@ -22,8 +26,27 @@ func Index(w http.ResponseWriter, req *http.Request){
 	}
 }
 
+func Search(newApi *news.Client) http.HandlerFunc {
+	return func (w http.ResponseWriter, req * http.Request){
+		u, err := url.Parse(req.URL.String())
+		if err != nil {
+			log.Fatalln(err.Error(), http.StatusInternalServerError)
+		}
+
+		params := u.Query()
+
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		fmt.Printf("The search query is %s", searchQuery)
+		fmt.Printf("The page is %s", page)
+	}
+}
+
 func main(){
-	router := mux.NewRouter()
 	err := godotenv.Load()
 	if err != nil{
 		log.Fatalln("Error loading .env file: ", err)
@@ -31,7 +54,21 @@ func main(){
 
 	port := os.Getenv("port")
 
-	http.Handle("/", router)
-	router.HandleFunc("/", Index)
-	panic(http.ListenAndServe(":"+port, router))
+	fs := http.FileServer(http.Dir("./assets/"))
+
+	apiKey := os.Getenv("NEWS_API_KEY")
+	if apiKey == ""{
+		log.Fatalln("ENV: API key must be set")
+	}
+
+	myClient := &http.Client{
+		Timeout:       10 * time.Second,
+	}
+	newsClient = news.NewClient(myClient, apiKey, 20)
+
+
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/search", Search(newsClient))
+	panic(http.ListenAndServe(":"+port, nil))
 }
